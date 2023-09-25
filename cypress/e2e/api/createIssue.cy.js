9827847d3925f5105f7582f8f5dce408ba7e6002
Sidney Pimentel <sidneyspe@gmail.com>
2023-09-25 10:04:04 -0300

    init project

---
+++ cypress/e2e/api/createIssue.cy.js
@@ -0,0 +1,22 @@
+import { faker } from "@faker-js/faker";
+
+describe("Create issue", () => {
+  beforeEach(() => cy.api_deleteProjects());
+
+  it("successfully", () => {
+    const issue = {
+      title: `issue-${faker.datatype.uuid()}`,
+      description: faker.random.words(3),
+      project: {
+        name: `project-${faker.datatype.uuid()}`,
+        description: faker.random.words(5),
+      },
+    };
+
+    cy.api_createIssue(issue).then((res) => {
+      expect(res.status).to.equal(201);
+      expect(res.body.title).to.equal(issue.title);
+      expect(res.body.description).to.equal(issue.description);
+    });
+  });
+});
---
+++ cypress/e2e/api/createProject.cy.js
@@ -0,0 +1,18 @@
+import { faker } from "@faker-js/faker";
+
+describe("Create Project", () => {
+  beforeEach(() => cy.api_deleteProjects());
+
+  it("successfully", () => {
+    const project = {
+      name: `project-${faker.datatype.uuid()}`,
+      description: faker.random.words(5),
+    };
+
+    cy.api_createProject(project).then((res) => {
+      expect(res.status).to.equal(201);
+      expect(res.body.name).to.equal(project.name);
+      expect(res.body.description).to.equal(project.description);
+    });
+  });
+});
